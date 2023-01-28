6a4b17830d7987c23b0f0163752b7f8f6b827d46
Artem <nikolloteslla@gmail.com>
2023-01-28 14:39:54 +0200

    main descktop

--- app/js/app.js
+++ app/js/app.js
@@ -11,4 +11,34 @@ $(function () {
     $(this).next().toggleClass('sub-menu--active');
   });
 
+  $('.slider-one').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 700,
+    arrows: true,
+    dots: true,
+    // responsive: [{
+    //     breakpoint: 1100,
+    //     settings: {
+    //       autoplay: false,
+    //     }
+    //   },
+    //   {
+    //     breakpoint: 700,
+    //     settings: {
+    //       arrows: false,
+    //     }
+    //   }
+    // ]
+  });
+
+  $(".rateYo").rateYo({
+    starWidth: "15px",
+    ratedFill: "#E94D6E",
+    spacing: "5px",
+    rating: 5,
+    readOnly: true
+  });
+
 });
